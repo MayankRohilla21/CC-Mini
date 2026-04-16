4e0d806fa8be0c1730a85e9529a96e6451062c11
MayankRohilla21 <pes2ug23am057@pesu.pes.edu>
2026-04-16 09:11:18 +0530

    Final integrated working project with failover and multi-client sync

--- replica/timers.js
+++ replica/timers.js
@@ -1,4 +1,4 @@
-    let electionTimeout;
+/*    let electionTimeout;
     let heartbeatInterval;
 
     let electionCallback = null;
@@ -44,3 +44,57 @@
     setElectionCallback,
     stopElectionTimer,
     };
+
+    */
+
+let electionTimeout;
+let heartbeatInterval;
+let electionCallback = null;
+
+function setElectionCallback(cb) {
+    electionCallback = cb;
+}
+
+function randomTimeout() {
+    return Math.floor(Math.random() * 300) + 500; // 500–800ms
+}
+function resetElectionTimer() {
+    clearTimeout(electionTimeout);
+
+    if (!electionCallback) {
+        console.error("Election callback not set!");
+        return;
+    }
+
+    const timeout = randomTimeout();
+    console.log(`[TIMER] Reset election timer: ${timeout}ms`);
+
+    electionTimeout = setTimeout(() => {
+        console.log("[TIMER] Election timeout reached");
+        electionCallback();
+    }, timeout);
+}
+
+function startHeartbeat(callback) {
+    clearInterval(heartbeatInterval);
+
+    heartbeatInterval = setInterval(() => {
+        callback();
+    }, 150);
+}
+
+function stopHeartbeat() {
+    clearInterval(heartbeatInterval);
+}
+
+function stopElectionTimer() {
+    clearTimeout(electionTimeout);
+}
+
+module.exports = {
+    resetElectionTimer,
+    startHeartbeat,
+    stopHeartbeat,
+    setElectionCallback,
+    stopElectionTimer,
+};
